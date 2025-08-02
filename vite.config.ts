463431c3169f8efe2038f9218c12fc6b5bd965bb
Maciek Sitkowski <msitkowski94@gmail.com>
2025-08-02 18:15:18 +0200

    replace eslint with biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/color-matcher/" : "/",
-  plugins: [react(), tailwindcss()],
-});
+	base: process.env.NODE_ENV === 'production' ? '/color-matcher/' : '/',
+	plugins: [react(), tailwindcss()],
+})
